e1c898008dd629a911974bd7daed3854c43a92f9
Aromero62823 <anro9968@colorado.edu>
2026-04-25 13:27:21 -0600

    Finished Final Project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,12 +4,14 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const axios = require('axios')
 
 public_users.post("/register", (req,res) => {
   //Write your code here
   const { username, password } = req.body;
   if(username && password) {
     if(isValid(username)) {
+      users.push({"username":username, "password":password})
       res.status(300).send('User registered!')
     } else {
       res.status(300).send(`Username ${username} already exists!`)
@@ -19,46 +21,85 @@ public_users.post("/register", (req,res) => {
   }
 });
 
+// Task 1/10
 // Get the book list available in the shop
-// public_users.get('/',function (req, res) {
-//   return res.status(300).send(JSON.stringify(books));
-// });
+public_users.get('/',function (req, res) {
+  return res.status(200).json(books);
+});
 
-public_users.get('/',async function (req, res) {
-  const books_data = await fetch(books);
-  
+public_users.get('/books', async (req, res) => {
+  try {
+    const response = await axios.get('http://localhost:5000/')
+    return res.status(200).json(response.data);
+  } catch {
+    return res.status(500).json({message: "Error"})
+  }
+});
 
-})
 
+// Task 2/11
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).send(JSON.stringify(books[req.params.isbn]));
- });
-  
+  return res.status(200).json(books[req.params.isbn]);
+});
+
+public_users.get('/isbn/:isbn/details', async (req, res) => {
+  try {
+    const response = await axios.get(`http://localhost:5000/isbn/${req.params.isbn}`)
+    return res.status(200).json(response.data)
+  } catch {
+    res.status(500).json({message: "Error"})
+  }
+})
+
+
+// Task 3/12
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
   const ret_books = Object.values(books).filter(book => (
     book.author == req.params.author
   ))
-  return res.status(300).send(ret_books);
+  return res.status(200).send(ret_books);
 });
 
+public_users.get('/author/:author/book', async (req, res) => {
+  try {
+      const response = await axios.get(`http://localhost:5000/author/${req.params.author}`)
+      return res.status(200).json(response.data);
+  } catch {
+    return res.status(500).json({message: "Error"})
+  }
+});
+
+
+// Task 4/13
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
   const ret_books = Object.values(books).filter(book => (
     book.title === req.params.title
   ))
-  return res.status(300).send(ret_books);
+  return res.status(200).send(ret_books);
 });
 
+public_users.get('/title/:title/book', async (req, res) => {
+  try {
+    const response = await axios(`http://localhost:5000/title/${req.params.title}`)
+    return res.status(200).send(response.data);
+  } catch {
+    return req.status(500).json({message: "Error"})
+  }
+})
+
+
+// Task 5
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
   const review_isbn = books[req.params.isbn].reviews
-  return res.status(300).send(review_isbn);
+  return res.status(200).send(review_isbn);
 });
 
 module.exports.general = public_users;
